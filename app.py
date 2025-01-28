6c5375b72bc85bef3a3d0b8c47f17eaff9edc107
henstyle <henspicsaypro@gmail.com>
2025-01-28 21:27:54 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, jsonify
 import subprocess
 
@@ -10,21 +11,19 @@ def home():
 @app.route('/run-script', methods=['POST'])
 def run_script():
     try:
-        # Ambil argumen dari permintaan POST
         data = request.get_json()
         argument = data.get('argument', '')
 
-        # Jalankan skrip dengan argumen
         result = subprocess.run(
-            ['./scripts/joko.sh', argument],  # Argumen ditambahkan di sini
+            ['./scripts/your_script.sh', argument],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
         return jsonify({
             'status': 'success',
-            'output': result.stdout,  # Output dari skrip
-            'error': result.stderr    # Jika ada error
+            'output': result.stdout,
+            'error': result.stderr
         })
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
